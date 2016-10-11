7cc3d405c56a9b08704c103f9d1eb38b7fb848ad
Luke Tillman <luke.tillman@gmail.com>
2016-10-11 07:49:17 -0600

    Add wait-for-it to subtree pull script

--- build/git-subtree-pull.ps1
+++ build/git-subtree-pull.ps1
@@ -4,3 +4,4 @@
 #>
 
 git subtree pull --prefix lib/killrvideo-data git@github.com:KillrVideo/killrvideo-data.git master --squash
+git subtree pull --prefix lib/wait-for-it git@github.com:vishnubob/wait-for-it.git master --squash
--- build/git-subtree-pull.sh
+++ build/git-subtree-pull.sh
@@ -2,3 +2,4 @@
 
 # Pull the latest from projects that are git subtrees
 git subtree pull --prefix lib/killrvideo-data git@github.com:KillrVideo/killrvideo-data.git master --squash
+git subtree pull --prefix lib/wait-for-it git@github.com:vishnubob/wait-for-it.git master --squash
