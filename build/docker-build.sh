6283e5b3a5a3faba32288bce805912e48186378c
Luke Tillman <luke.tillman@gmail.com>
2016-10-12 09:09:08 -0600

    Try to fix dot sourcing in build script

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 
 # Load the version number
-source "`dirname $0`/VERSION"
+. "`dirname $0`/VERSION"
 
 # Build with docker
 docker build -t killrvideo/killrvideo-dse:$DOCKER_BUILD_TAG .
